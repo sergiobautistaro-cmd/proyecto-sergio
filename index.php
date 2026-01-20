f513a2fd2d0ecca9f3e707353ed3e29c5c5611c5
Sergio <sergiobautistaro@gmail.com>
2026-01-20 11:30:19 +0100

    Corregido error de sintaxis en index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 echo '<?php
 $numero = $argv[1];
 if ($numero % 2 == 0) {
-    echo "El numero $numero es par.\n";
+    echo "El numero $numero es par." .PHP_EOL;
 } else {
-    echo "El numero $numero es impar.\n";
-}' > index.php
+    echo "El numero $numero es impar." . PHP_EOL;
+}
+?>' > index.php
 
 echo "# proyecto-sergio" > README.md
