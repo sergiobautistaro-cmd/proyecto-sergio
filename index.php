4e63aefa84e54163989bd86769a90f4a33884ecc
Sergio <sergiobautistaro@gmail.com>
2026-01-20 11:08:02 +0100

    Proyecto final: PHP par/impar y README

---
+++ index.php
@@ -0,0 +1,9 @@
+echo '<?php
+$numero = $argv[1];
+if ($numero % 2 == 0) {
+    echo "El numero $numero es par.\n";
+} else {
+    echo "El numero $numero es impar.\n";
+}' > index.php
+
+echo "# proyecto-sergio" > README.md
